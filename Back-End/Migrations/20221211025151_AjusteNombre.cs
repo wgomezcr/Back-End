5483222563ea1cb11ae42791df148b0d52cdee25
wgomezcr <walter.gomez.cr@gmail.com>
2022-12-11 21:04:49 -0600

    Leccion 114 completa

---
+++ Back-End/Migrations/20221211025151_AjusteNombre.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PeliculasApi.Migrations
+{
+    public partial class AjusteNombre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FechaNacimineto",
+                table: "Actores",
+                newName: "FechaNacimiento");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FechaNacimiento",
+                table: "Actores",
+                newName: "FechaNacimineto");
+        }
+    }
+}
